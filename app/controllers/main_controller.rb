f42c0fe3c577822b21a20c684904ee7f1d7cd0a1
Aziz Harrington <azizharrington@gmail.com>
2013-06-23 13:10:48 -0500

    updated create incident responder in main controller

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -20,6 +20,27 @@ class MainController < ApplicationController
   end
 
   def create_incident_responder
+    dispatcher = params[:dispatcher]
+    #dispatched_date = params[:dispatched_date]
+    dispatched_time = params[:dispatched_time]
+    #date_on_scene = params[:date_on_scene]
+    #time_on_scene = params[:time_on_scene]
+    #date_left_scene = params[:date_left_scene]
+    #time_left_scene = params[:time_left_scene]
+    #initial_responder = params[:initial_responder]
+    ###incident_id
+    ###staff_id
+
+    IncidentResponder.create(#date_left_scene: date_left_scene, 
+                             #date_on_scene: date_on_scene, 
+                             #dispatched_date: dispatched_date, 
+                             dispatched_time: dispatched_time, 
+                             dispatcher_id: dispatcher, 
+                             #incident_id:, 
+                             #initial_responder: initial_responder, 
+                             #staff_id:, 
+                             #time_left_scene: time_left_scene,
+                             #time_on_scene: time_on_scene)
 
   end
 
